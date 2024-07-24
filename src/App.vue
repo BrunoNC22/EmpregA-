91f0836ca8c446c2e353ade23495baddfc0ae2c1
Miott0 <miotterafael@gmail.com>
2024-07-24 08:29:30 -0300

    feat: creat component navigation

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
+    import Navigation from './components/Navigation.vue';
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <main>
+        <Navigation class="navbar"/>
+        
+        <section>
+            <p>Bem vindo, <strong>Matheus</strong></p>
+        </section>
+         
+
+    </main>
+    
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+    main{
+        background-color: #F5F7FB;
+        height: 100vh;
+        
+
+    }
+
+    section{
+        padding: 24px;
+        background-color: aquamarine;
+    }
+    
+
+    .navbar{
+        width: 100vw;
+        background-color: aqua;
+        
+    }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav class="navmenu">
+        <div>
+            Logo
+        </div>
+
+
+        <div class="options">
+            <div class="searchIcon">O</div>
+            <div class="menu">M</div>
+        </div>
+    </nav>
+</template>
+
+<style >
+    .navmenu{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+
+        padding: 24px 24px;
+       
+
+    }
+
+    .options{
+        display: flex;
+        justify-content: space-around;
+        width: 15%;
+        gap: 24px;
+    }
+</style>
