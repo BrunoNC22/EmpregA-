18bb53e031bbfbe4b84df4743247b8d0488c0aee
BrunoNC22 <thebegone2@gmail.com>
2024-07-26 11:04:51 -0300

    feat: remove test code

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,13 @@
 <script setup lang="ts">
-
+    import {RouterView} from 'vue-router'
     import Navigation from './components/Navigation.vue';
 
 
 </script>
 
 <template>
-    <main>
-        <Navigation class="navbar"/>
-        
-        <section>
-            <p>Bem vindo, <strong>Matheus</strong></p>
-        </section>
-         
-
-    </main>
-    
+    <RouterView />
 </template>
 
 <style scoped>
-    main{
-        background-color: #F5F7FB;
-        height: 100vh;
-        
-
-    }
-
-    section{
-        padding: 24px;
-        background-color: aquamarine;
-    }
-    
-
-    .navbar{
-        width: 100vw;
-        background-color: aqua;
-        
-    }
 </style>
