4986a41f409bc840572803869e9dc8ce9453a21a
BrunoNC22 <thebegone2@gmail.com>
2024-08-27 01:26:00 -0300

    feat: use tanstack query plugin

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import './style/main.css'
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
+import {VueQueryPlugin } from '@tanstack/vue-query'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(VueQueryPlugin)
+
+app.mount('#app')
