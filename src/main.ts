27ca5f37fdd7a0a3da08a555d4f6ce885a70202d
BrunoNC22 <thebegone2@gmail.com>
2024-08-29 14:14:11 -0300

    feat: install prime vue component lib

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,19 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 import {VueQueryPlugin } from '@tanstack/vue-query'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
 const app = createApp(App)
 app.use(router)
 app.use(VueQueryPlugin)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.dark-mode' // para habilitar o dark mode essa classe dark-mode tem que ser inserida na tag html
+    }
+  }
+})
 
 app.mount('#app')
