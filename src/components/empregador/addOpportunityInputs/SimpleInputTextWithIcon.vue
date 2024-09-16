48c8ba9d297eec72332525d75b31dbbf3fc09ba9
BrunoNC22 <thebegone2@gmail.com>
2024-09-16 20:35:40 -0300

    feat: create simple input text with icon component

---
+++ src/components/empregador/addOpportunityInputs/SimpleInputTextWithIcon.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps({
+  title: {
+    required: true,
+    type: String
+  },
+  placeholder: {
+    required: false,
+    default: '',
+    type: String
+  },
+  helpText: {
+    required: true,
+    type: String
+  },
+  model: {
+    required: true,
+  },
+  errors: {
+    required: false,
+  },
+  required: {
+    required: false,
+    default: false,
+    type: Boolean
+  }
+})
+
+const emit = defineEmits(['update:model'])
+
+// Função para limpar a string e converter para número.
+// Essa função é necessária pois o retorno do input vem no formato 'R$ 000,000.00'
+const cleanCurrencyString = (value: string): number => {
+  if (!value) return 0
+  // Remove o símbolo de moeda e separadores, mantendo apenas números e ponto decimal
+  const cleanString = value.replace(/[^\d,.-]/g, '').replace(',', '.');
+  
+  return parseFloat(cleanString);
+}
+
+const updateModel = (newValue: string) => {
+  const number = cleanCurrencyString(newValue)
+  emit('update:model', number)
+}
+</script>
+<template>
+  <div class="flex flex-col gap-2">
+    <label :for="`currency${title}`">
+      {{ title }}
+      <b v-if="required" style="color: red;" >*</b>
+    </label>
+    <InputNumber v-bind:model-value="model" @blur="($event) => updateModel($event.value)" :placeholder="placeholder" :input-id="`currency${title}`" :mode="'currency'" :currency="'BRL'" :locale="'pt-BR'" fluid />
+    <small :id="`username-help${title}`">{{ helpText }}</small>
+    <small class="error-help" v-for="error of errors" :key="error.$uid" >{{ error.$message }} </small>
+  </div>
+</template>
+<style scoped>
+.error-help {
+  color: red;
+}
+</style>
